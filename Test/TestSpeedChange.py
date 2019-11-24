bae56b18cf71c66a88a73976b31dca70bd176a90
efode <efidesalew@gmail.com>
2019-11-24 08:56:18 -0500

    Audio Effects modified

--- Test/TestSpeedChange.py
+++ Test/TestSpeedChange.py
@@ -1,3 +1,20 @@
+"""
+    <ed-Audio is an open source audio processing toolbox>
+    Copyright (C) <2019>  <Efriem Desalew Gebie>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
 from Utilities import DirectoryManager
 from Utilities import FileManager
 from EffectsManager import EffectsManager
@@ -5,7 +22,7 @@ from EffectsManager import EffectsManager
 # define the path for source directory
 source_dir = "../../Test_Data/Reference_Audios"
 # define destination path for modified audios
-dist_dirc = "../../Test_Data/Modified_Audios/Speed_Change"
+dist_dir = "../../Test_Data/Modified_Audios/Speed_Change"
 # search for all .mp3 files under the specified source directory
 mp3_files = DirectoryManager.find_mp3_files(source_dir=source_dir)
 # apply speed change on the first audio file
@@ -16,14 +33,14 @@ audio_name = original_audio_path.split("/")[4].split(".")[0]
 print("Original Audio Path: ", original_audio_path)
 # applying 10% speed change to for an audio portion specified with offset and duration parameters
 EffectsManager.apply_speed_change(original_audio_path=original_audio_path,
-                                  modified_audio_path=dist_dirc+"/"+audio_name+str(100+10)+".wav",
+                                  modified_audio_path=dist_dir + "/" + audio_name + str(100 + 10) + ".wav",
                                   target_speed_in_percent=10,
                                   offset=10.0,
                                   duration=20.0
                                   )
 # applying -10% speed change to an audio portion specified with offset and duration parameters
 EffectsManager.apply_speed_change(original_audio_path=original_audio_path,
-                                  modified_audio_path=dist_dirc+"/"+audio_name+str(100-10)+".wav",
+                                  modified_audio_path=dist_dir + "/" + audio_name + str(100 - 10) + ".wav",
                                   target_speed_in_percent=-10,
                                   offset=10.0,
                                   duration=20.0
