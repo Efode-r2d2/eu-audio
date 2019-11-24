d6348360b63db6df54972e4448514c1d58189c0e
efode <efidesalew@gmail.com>
2019-11-24 10:46:59 -0500

    White Noise Test Class

--- Test/TestWhiteNoise.py
+++ Test/TestWhiteNoise.py
@@ -15,3 +15,36 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
+from Utilities import DirectoryManager
+from EffectsManager import EffectsManager
+
+# source directory
+src_dir = "../../Test_Data/Reference_Audios"
+# destination directory for modified audios
+dist_dir = "../../Test_Data/Modified_Audios/White_Noise"
+# searching for all .mp3 files in a given source directory
+mp3_files = DirectoryManager.find_mp3_files(source_dir=src_dir)
+# adding white noise to the fourth audio
+original_audio_path = mp3_files[3]
+file_name = original_audio_path.split("/")[4].split(".")[0]
+print("Original Audio Path: ", original_audio_path)
+print("File Name: ", file_name)
+# adding white noise with target signal to noise ratio of
+# -10 dB to a portion of audio specified by offset and
+# duration parameters
+EffectsManager.add_white_noise(original_audio_path=original_audio_path,
+                               modified_audio_path=dist_dir+"/"+file_name+str(-10)+".wav",
+                               target_snr_in_db=-10,
+                               offset=10.0,
+                               duration=20.0)
+print("Modified Audio Saved Here: ", dist_dir+"/"+file_name+str(-10)+".wav")
+# adding white noise with target signal to noise ratio of
+# 10 dB to a portion of audio specified by offset and
+# duration parameters
+EffectsManager.add_white_noise(original_audio_path=original_audio_path,
+                               modified_audio_path=dist_dir+"/"+file_name+str(10)+".wav",
+                               target_snr_in_db=10,
+                               offset=10.0,
+                               duration=20.0)
+print("Modified Audio Saved Here: ", dist_dir+"/"+file_name+str(10)+".wav")
+
