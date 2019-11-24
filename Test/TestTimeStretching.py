9a706cad98ac68802772abe998564fc7d228ef27
efode <efidesalew@gmail.com>
2019-11-24 10:00:41 -0500

    Audio Effects modified

--- Test/TestTimeStretching.py
+++ Test/TestTimeStretching.py
@@ -16,3 +16,31 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from EffectsManager import EffectsManager
+from Utilities import DirectoryManager
+# source directory
+src_dir = "../../Test_Data/Reference_Audios"
+# destination directory path for modified audios
+dist_dir = "../../Test_Data/Modified_Audios/Time_Stretched"
+# find all .mp3 files under given source directory
+mp3_files = DirectoryManager.find_mp3_files(source_dir=src_dir)
+# applying time stretching to the second audio in .mp3 files list
+original_audio_path = mp3_files[1]
+file_name = original_audio_path.split("/")[4].split(".")[0]
+print("Original Audio Path: ", original_audio_path)
+print("File Name", file_name)
+# applying 10% time stretching on a portion of audio defined by offset and duration parameters
+EffectsManager.apply_time_stretching(original_audio_path=original_audio_path,
+                                     modified_audio_path=dist_dir+"/"+file_name+str(100+10)+".wav",
+                                     target_duration_in_percent=10,
+                                     offset=10.0,
+                                     duration=20.0)
+print("Audio Saved Here, ", dist_dir+"/"+file_name+str(100+10)+".wav")
+# applying -10% time stretching on a portion of audio defined by offset and duration parameters
+EffectsManager.apply_time_stretching(original_audio_path=original_audio_path,
+                                     modified_audio_path=dist_dir+"/"+file_name+str(100-10)+".wav",
+                                     target_duration_in_percent=-10,
+                                     offset=10.0,
+                                     duration=20.0)
+print("Audio Saved Here, ", dist_dir+"/"+file_name+str(100-10)+".wav")
+
+
