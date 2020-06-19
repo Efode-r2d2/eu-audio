ef5168e2a3b9d9dc97f7bbd2fcae702295b2b5c2
efode-r2d2 <efidesalew@gmail.com>
2020-06-19 15:25:56 +0300

    new test script for bulk speed change

--- Test/TestBulkSpeedChange.py
+++ Test/TestBulkSpeedChange.py
@@ -16,33 +16,21 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 from Utilities import DirectoryManager
-from Utilities import FileManager
 from EffectsManager import EffectsManager
 
-# source directory
-source_dir = "../../Test_Data/Reference_Audios"
-# dis directory
-dist_dir = "../../Test_Data/Query_Audios/Speed_Change"
-# all reference audios, .mp3 format
-mp3_files = DirectoryManager.find_mp3_files(source_dir=source_dir)
-# apply speed change on the first audio file
-original_audio_path = mp3_files[0]
-print(original_audio_path.split("/"))
-audio_name = original_audio_path.split("/")[4].split(".")[0]
-# modified and original audio paths
-print("Original Audio Path: ", original_audio_path)
-# applying 10% speed change to for an audio portion specified with offset and duration parameters
-speed_change_in_percent = 0
-EffectsManager.apply_speed_change(original_audio_path=original_audio_path,
-                                  modified_audio_path=dist_dir + "/" + audio_name + "_Speed_" + str(speed_change_in_percent) + ".wav",
-                                  target_speed_in_percent=speed_change_in_percent,
-                                  offset=10.0,
-                                  duration=5.0
-                                  )
-# applying -10% speed change to an audio portion specified with offset and duration parameters
-'''EffectsManager.apply_speed_change(original_audio_path=original_audio_path,
-                                  modified_audio_path=dist_dir + "/" + audio_name + str(100 - 10) + ".wav",
-                                  target_speed_in_percent=-10,
-                                  offset=10.0,
-                                  duration=40.0
-                                  )'''
+# source directory of reference audios
+ref_audios_dir = "../../Test_Data/Ref_Audios"
+# destination directory of query audios
+query_audio_dir = "../../Test_Data/Query_Audios/Speed_Change"
+# reference audios
+reference_audios = DirectoryManager.find_wav_files(source_dir=ref_audios_dir)
+# applying pitch shifting for set of reference audio files
+for i in reference_audios:
+    audio_name = i.split("/")[4]
+    for j in range(-30, 32, 2):
+        DirectoryManager.create_dir(source_dir=query_audio_dir + "/" + str(100 + j))
+        EffectsManager.apply_speed_change(original_audio_path=i,
+                                          modified_audio_path=query_audio_dir + "/" + str(100 + j) + "/" + audio_name,
+                                          target_speed_in_percent=j,
+                                          offset=None,
+                                          duration=None)
